b3ff13b22e6e00d92b3f670839dc17a85f7bb749
adamjuwig <adamjuwig@gmail.com>
2021-04-02 13:01:38 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget.development", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false 
 });
